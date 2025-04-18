7d0b40af54b25dd3065f377e01c8fd758f9e3729
VaibhavSharma8764 <raghavsharma.8764@gmail.com>
2025-04-19 01:07:32 +0530

    Update globalContext.js

--- frontend/src/context/globalContext.js
+++ frontend/src/context/globalContext.js
@@ -1,7 +1,7 @@
 import React, { useContext, useState } from "react";
 import axios from "axios";
 
-const BASE_URL = "http://localhost:3000/api/v1/";
+const BASE_URL = "https://expense-tracker-fullstack-master.onrender.com";
 
 const GlobalContext = React.createContext();
 
